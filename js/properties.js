b0bde526b61b40408e65bb9d21643530f63d3a64
Joaquin De Sosa <joaquindesosa@gmail.com>
2022-01-09 19:13:32 -0300

    filter toggle button fixed

--- js/properties.js
+++ js/properties.js
@@ -2,12 +2,17 @@ let filterToggleButton = document.getElementById("filter-button")
 let searchButton = document.getElementById("search-button")
 let asideBar = document.getElementsByTagName("aside")[0]
 
+let sectionContainer = document.getElementById("section-container")
 let propertiesContainer = document.getElementById("properties-container")
 
 function showProperties(){
     propertiesContainer.innerHTML = ""
     printHouses(houses, propertiesContainer)
 }
+
+sectionContainer.addEventListener('click', function(){
+    asideBar.classList.toggle("hidden")
+})
 
 searchButton.addEventListener('click', function(){
     asideBar.classList.toggle("hidden")
